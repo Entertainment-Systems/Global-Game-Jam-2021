5d5da87ff4171585ca670af3166ca96d64bae87e
Przemyslaw Pokorski <Przemyslaw_Pokorski@dell.com>
2021-01-29 21:09:03 +0000

    Pills only pick up when colliding with player

--- Assets/Scripts/Pill.cs
+++ Assets/Scripts/Pill.cs
@@ -8,8 +8,11 @@ public class Pill : MonoBehaviour
     public float pillValue = .2f;
     private void OnTriggerEnter(Collider other)
     {
-        //Add pills value to the current high
-        GameEvents.current.OnOnPillPicked(pillValue);
-        Destroy(this);
+        if(other.CompareTag("Player"))
+        {
+            //Add pills value to the current high
+            GameEvents.current.OnOnPillPicked(pillValue);
+            Destroy(this);
+        }
     }
 }
