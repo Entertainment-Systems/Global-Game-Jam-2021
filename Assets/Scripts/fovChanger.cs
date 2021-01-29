bc078f4b23e4ce7dc4fcc1c9e9e031c807bf67f2
Ciarán Malone <ciaranmalone27@gmail.com>
2021-01-29 22:01:53 +0000

    Fov script added and scene name change

---
+++ Assets/Scripts/fovChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fovChanger : MonoBehaviour
+{
+    float high;
+
+    void Start()
+    {
+        GameEvents.current.onPillPicked += OnPillPickUp;
+    }
+
+    private void OnPillPickUp(float high)
+    {
+        high = GameStates.current.High;
+        Camera.main.fieldOfView = 120;
+        Debug.Log("High A F" + high);
+    }
+}
