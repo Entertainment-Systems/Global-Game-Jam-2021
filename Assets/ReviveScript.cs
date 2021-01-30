2662b8ee602eee9a5eebc9aabaa515ab0f668aff
Ciarán Malone <ciaranmalone27@gmail.com>
2021-01-30 22:35:00 +0000

    Death comes to us all

---
+++ Assets/ReviveScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ReviveScript : MonoBehaviour
+{
+    [SerializeField] private Transform teleportPoint;
+    [SerializeField] public float teleportTime = .8f;
+
+
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+
+        GameEvents.current.PlayerLostLife += TeleportPlayer;
+    }
+
+   void TeleportPlayer(int liveLeft)
+    {
+        coroutine = movePlayer(teleportTime);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator movePlayer(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        transform.position = teleportPoint.position;
+    }
+}
